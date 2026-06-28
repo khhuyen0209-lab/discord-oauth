757b626c1d4afe2a6f88cfeafabd918e6d4d21d3
khhuyen0209-lab <khhuyen0209@gmail.com>
2026-06-28 11:51:42 +0700

    Fix Railway host

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ app.get("/auth/discord/callback", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server chạy ở cổng ${PORT}`);
 });
+
